049ed70eb55f35155968f76c98e975a76a026ade
Ghost <jessiecodes@gmail.com>
2017-07-24 18:25:28 -0400

    cleanup and failing test to start from

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -11,11 +11,19 @@ describe BowlingGame do
     game.roll(8)
     expect(game.score?).to eq(8)
   end
-    it "returns 8 when 8,1 is thrown" do
+  it "returns 9 when 8,1 is thrown" do
     game = BowlingGame.new
     game.roll(8)
     game.roll(1)
     expect(game.score?).to eq(9)
+  end
+
+  it "returns 28 for 9/, 90" do
+    game = BowlingGame.new
+    game.roll(9)
+    game.roll(1)
+    game.roll(9)
+    expect(game.score?).to eq(28)
   end
 
 end 
