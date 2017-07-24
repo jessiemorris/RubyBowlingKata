758decc4004752704d71190d82127bf6c1dbb514
Ghost <jessiecodes@gmail.com>
2017-07-24 18:14:19 -0400

    Prototyping up to simple two throw frames

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,11 +1,26 @@
 class BowlingGame
   def initialize
     @frames = []
+    @frameCount = 1
+    @rollCount = 0
+    @currentPinsDown = 0
   end
   def roll(pinsDown)
-    @frames.push(8)
+    if @rollCount == 0 then
+      @currentPinsDown = pinsDown
+      @rollCount += 1
+    else
+      @currentPinsDown += pinsDown
+      @frames.push(@currentPinsDown)
+      initFrame
+    end
   end
   def score? 
-    @frames.first
+    @frames.reduce(0, :+) + @currentPinsDown
+  end
+  def initFrame
+    @rollCount = 0
+    @frameCount += 1
+    @currentPinsDown = 0
   end
 end
--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -2,10 +2,20 @@ require_relative '../lib/bowling_game.rb'
 
 describe BowlingGame do
 
-  it "returns 0 when asked for score" do
+  it "returns 0 when asked for score of a new game" do
+    expect(BowlingGame.new.score?).to eq(0)
+  end
+
+  it "returns 8 when a single 8 is thrown" do
     game = BowlingGame.new
     game.roll(8)
     expect(game.score?).to eq(8)
+  end
+    it "returns 8 when 8,1 is thrown" do
+    game = BowlingGame.new
+    game.roll(8)
+    game.roll(1)
+    expect(game.score?).to eq(9)
   end
 
 end 
