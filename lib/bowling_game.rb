5b6c9f7dc5dbaff96146ab4f0947d77565c88265
Ghost <jessiecodes@gmail.com>
2017-07-25 20:12:26 -0400

    Simplified logic since we're not using frames yet, might not need them

--- lib/bowling_game.rb
+++ lib/bowling_game.rb
@@ -1,26 +1,14 @@
 class BowlingGame
   def initialize
-    @frames = []
+    @rolls = []
     @frameCount = 1
     @rollCount = 0
     @currentPinsDown = 0
   end
   def roll(pinsDown)
-    if @rollCount == 0 then
-      @currentPinsDown = pinsDown
-      @rollCount += 1
-    else
-      @currentPinsDown += pinsDown
-      @frames.push(@currentPinsDown)
-      initFrame
-    end
+    @rolls.push(pinsDown)
   end
   def score? 
-    @frames.reduce(0, :+) + @currentPinsDown
-  end
-  def initFrame
-    @rollCount = 0
-    @frameCount += 1
-    @currentPinsDown = 0
+    @rolls.reduce(0, :+)
   end
 end
